523d5e2ad6b773eb2bbfbdfec61916f08bfa0f17
toufiq007 <t.limon2342@gmail.com>
2024-02-21 13:24:55 +0600

    express validator package added for mongoose field validation

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,41 +1,50 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
 import jwt from "jsonwebtoken";
+import { validationResult } from "express-validator";
 
 const userRegister = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: errors.array(),
+    });
+  }
   try {
-    const { email, name, password, confirm_password } = req.body;
+    const { email } = req.body;
     let user = await User.findOne({ email: email });
     if (user) {
       res.status(400).json({
-        message: "email already exist!!",
-      });
-    } else {
-      user = new User(req.body);
-      await user.save();
-
-      //! generate jwt token
-      const token = jwt.sign(
-        { userId: user._id },
-        process.env.JWT_SECRET_KEY as string,
-        { expiresIn: "2d" }
-      );
-
-      //! set token to the cookies
-      res.cookie("auth_token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 86400000,
-      });
-      res.status(201).json({
-        message: "user created successfully",
-        user,
+        message: "User is already exists",
       });
     }
-  } catch (errr) {
-    res.status(500).json({
-      message: "Something went wrong!!",
-      error: errr,
+    user = new User(req.body);
+    await user.save();
+
+    //! generate jwt token
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY as string,
+      { expiresIn: "2d" }
+    );
+
+    //! set http cookie
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+
+    // send response to frontend
+    return res.status(200).json({
+      message: "user created successfully",
+      user: user,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Something went Wrong!",
     });
   }
 };
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,8 +1,23 @@
-import express, { Request } from "express";
+import express, { Request, Response } from "express";
 import userController from "../controllers/user.controller";
+import { check } from "express-validator";
+import User from "../models/user.model";
+import jwt from "jsonwebtoken";
+import { validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/register", userController.userRegister);
+router.post(
+  "/register",
+  [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  userController.userRegister
+);
 
 export default router;
