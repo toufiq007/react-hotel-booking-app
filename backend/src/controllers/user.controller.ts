0a0f8954089640deaffaa71094667bb57560f186
toufiq007 <t.limon2342@gmail.com>
2024-02-22 01:02:45 +0600

    fixed some issue on userLogin api

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -57,36 +57,34 @@ const userRegister = async (req: Request, res: Response) => {
 export const userLogin = async (req: Request, res: Response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.status(400).json({
-      message: errors.array(),
-    });
+    return res.status(400).json({ message: errors.array() });
   }
+
+  const { email, password } = req.body;
   try {
-    // all of login functionality
-    const { email, password } = req.body;
+    // const user = await User.findOne({email:email})
     const user = await User.findOne({ email: email });
     if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch && email !== user.email) {
       res.status(400).json({ message: "invalid credentials" });
     }
-    const isMathced = await bcrypt.compare(password, user?.password as string);
-    if (isMathced && email === user?.email) {
-      // generate new token
-      const token = generateNewToken(user?._id as string);
+    const token = generateNewToken(user._id);
 
-      // save token to httpCookie
-      res.cookie("auth_token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 86400000,
-      });
-
-      res.status(200).json({ message: "User logged in successfull!",userid:user?._id });
-    } else {
-      res.status(400).json({ message: "invalid credentials" });
-    }
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+    return res.status(200).json({
+      message: "User logged in successfully",
+      id: user._id,
+    });
   } catch (err) {
-    res.status(400).json({
-      message: "Something went Wrong!!",
+    return res.status(500).json({
+      message: "Internal server error",
     });
   }
 };
